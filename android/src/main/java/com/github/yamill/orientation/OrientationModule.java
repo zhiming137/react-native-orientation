b98810176b49777c7d523b382e36a8f9559e960e
wangzhiming <zhiming137@gmail.com>
2017-04-27 17:59:29 +0800

    修改android的实现方式

--- android/src/main/java/com/github/yamill/orientation/OrientationModule.java
+++ android/src/main/java/com/github/yamill/orientation/OrientationModule.java
@@ -7,7 +7,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.media.ExifInterface;
 import android.util.Log;
+import android.view.OrientationEventListener;
+import android.view.Surface;
+import android.view.WindowManager;
 
 import com.facebook.common.logging.FLog;
 import com.facebook.react.bridge.Arguments;
@@ -25,27 +29,53 @@ import java.util.Map;
 
 import javax.annotation.Nullable;
 
-public class OrientationModule extends ReactContextBaseJavaModule implements LifecycleEventListener{
-    final BroadcastReceiver receiver;
+class OrientationModule extends ReactContextBaseJavaModule implements LifecycleEventListener{
+    private static final String TAG = "OrientationModule";
+    private OrientationEventListener mOrientationListener; // 屏幕方向改变监听器
+
+    private int screenOrientation;
 
     public OrientationModule(ReactApplicationContext reactContext) {
         super(reactContext);
         final ReactApplicationContext ctx = reactContext;
 
-        receiver = new BroadcastReceiver() {
+
+        mOrientationListener = new OrientationEventListener(ctx) {
             @Override
-            public void onReceive(Context context, Intent intent) {
-                Configuration newConfig = intent.getParcelableExtra("newConfig");
-                Log.d("receiver", String.valueOf(newConfig.orientation));
+            public void onOrientationChanged(int i) {
+                // i的范围是0～359
+                // 屏幕左边在顶部的时候 i = 90;
+                // 屏幕顶部在底部的时候 i = 180;
+                // 屏幕右边在底部的时候 i = 270;
+                // 正常情况默认i = 0;
 
-                String orientationValue = newConfig.orientation == 1 ? "PORTRAIT" : "LANDSCAPE";
+                int mScreenExifOrientation;
+                if (45 <= i && i < 135) {
+                    mScreenExifOrientation = ExifInterface.ORIENTATION_ROTATE_90;
+                } else if (135 <= i && i < 225) {
+                    mScreenExifOrientation = ExifInterface.ORIENTATION_ROTATE_180;
+                } else if (225 <= i && i < 315) {
+                    mScreenExifOrientation = ExifInterface.ORIENTATION_ROTATE_270;
+                } else {
+                    mScreenExifOrientation = ExifInterface.ORIENTATION_NORMAL;
+                }
 
-                WritableMap params = Arguments.createMap();
-                params.putString("orientation", orientationValue);
-                if (ctx.hasActiveCatalystInstance()) {
-                    ctx
-                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                    .emit("orientationDidChange", params);
+                if (OrientationModule.this.screenOrientation != mScreenExifOrientation) {
+                    OrientationModule.this.screenOrientation = mScreenExifOrientation;
+
+                    String orientation = OrientationModule.this.getOrientationString();
+                    String specificOrientation = OrientationModule.this.getSpecificOrientationString();
+
+                    WritableMap params = Arguments.createMap();
+                    params.putString("orientation", orientation);
+
+                    WritableMap specificParams = Arguments.createMap();
+                    specificParams.putString("specificOrientation", specificOrientation);
+
+                    ctx.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                            .emit("orientationDidChange", params);
+                    ctx.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                            .emit("specificOrientationDidChange", specificParams);
                 }
             }
         };
@@ -59,12 +89,23 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
 
     @ReactMethod
     public void getOrientation(Callback callback) {
-        final int orientationInt = getReactApplicationContext().getResources().getConfiguration().orientation;
 
-        String orientation = this.getOrientationString(orientationInt);
+        String orientation = this.getOrientationString();
 
         if (orientation == "null") {
-            callback.invoke(orientationInt, null);
+            callback.invoke(null, null);
+        } else {
+            callback.invoke(null, orientation);
+        }
+    }
+
+    @ReactMethod
+    public void getSpecificOrientation(Callback callback) {
+
+        String orientation = this.getSpecificOrientationString();
+
+        if (orientation == "null") {
+            callback.invoke(null, null);
         } else {
             callback.invoke(null, orientation);
         }
@@ -118,9 +159,8 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
     @Override
     public @Nullable Map<String, Object> getConstants() {
         HashMap<String, Object> constants = new HashMap<String, Object>();
-        int orientationInt = getReactApplicationContext().getResources().getConfiguration().orientation;
 
-        String orientation = this.getOrientationString(orientationInt);
+        String orientation = this.getOrientationString();
         if (orientation == "null") {
             constants.put("initialOrientation", null);
         } else {
@@ -130,24 +170,35 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
         return constants;
     }
 
-    private String getOrientationString(int orientation) {
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+    private String getOrientationString() {
+        String orientationString = this.getSpecificOrientationString();
+        if (orientationString == "LANDSCAPE-RIGHT" || orientationString == "LANDSCAPE-LEFT") {
             return "LANDSCAPE";
-        } else if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+        }
+        return orientationString;
+    }
+
+    private String getSpecificOrientationString() {
+        if (screenOrientation == ExifInterface.ORIENTATION_NORMAL) {
             return "PORTRAIT";
-        } else if (orientation == Configuration.ORIENTATION_UNDEFINED) {
-            return "UNKNOWN";
+        } else if (screenOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return "LANDSCAPE-RIGHT";
+        } else if (screenOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return "PORTRAITUPSIDEDOWN";
+        } else if (screenOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return "LANDSCAPE-LEFT";
         } else {
-            return "null";
+            return "UNKNOWN";
         }
     }
 
     @Override
     public void onHostResume() {
         final Activity activity = getCurrentActivity();
+        Log.d(TAG, "onHostResume: ");
 
         assert activity != null;
-        activity.registerReceiver(receiver, new IntentFilter("onConfigurationChanged"));
+        mOrientationListener.enable();
     }
     @Override
     public void onHostPause() {
@@ -155,7 +206,8 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
         if (activity == null) return;
         try
         {
-            activity.unregisterReceiver(receiver);
+            Log.d(TAG, "onHostPause: ");
+            mOrientationListener.disable();
         }
         catch (java.lang.IllegalArgumentException e) {
             FLog.e(ReactConstants.TAG, "receiver already unregistered", e);
@@ -168,9 +220,12 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
         if (activity == null) return;
         try
         {
-            activity.unregisterReceiver(receiver);
+            Log.d(TAG, "onHostDestroy: ");
+            mOrientationListener.disable();
         }
         catch (java.lang.IllegalArgumentException e) {
             FLog.e(ReactConstants.TAG, "receiver already unregistered", e);
-        }}
-    }
+        }
+	}
+}
+
